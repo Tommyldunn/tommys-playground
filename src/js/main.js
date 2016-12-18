6c77ab6008724076fd6cba00f4fa90494875a7cf
tommyldunn <tommylogandunn@gmail.com>
2016-12-17 20:37:02 -0500

    more

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,1 @@
-console.log('%c app is running %c🚀', 'color: dodgerblue; font-weight: bold; font-size: 20px', 'font-size: 30px');
+console.log('%c playground app is ready %c', 'color: dodgerblue; font-weight: bold; font-size: 12px', 'font-size: 30px');
